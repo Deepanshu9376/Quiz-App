a062e10fa1af0dad66a6e60defa785c62f4322af
Deepanshu <tdeepanshu80@gmail.com>
2024-02-28 17:33:17 +0530

    updated the scriptjs

--- script.js
+++ script.js
@@ -24,16 +24,58 @@ continuebtn.onclick=()=>{
     quizBox.classList.add('active')
 
     showQuestions(0);
+    questionCounter(1);
 }
 
 let questionCount=0;
+let questionNumb=1;
+
 const nextbtn=document.querySelector('.next-btn');
+
 nextbtn.onclick=()=>{
-    questionCount++;
-    showQuestions(questionCount);
+    if(questionCount<questions.length-1){
+        questionCount++;
+        showQuestions(questionCount);
+
+        questionNumb++;
+        questionCounter(questionNumb);
+    }
+    else{
+        console.log("questions Completed");
+    }
 }
+
+const optionList=document.querySelector('.option-list');
 
 function showQuestions(index){
     const questionText=document.querySelector('.question-text');
     questionText.textContent=`${questions[index].numb} ${questions[index].question}`;
+
+    let optionTag=`<div class="option"><span>${questions[index].options[0]}</span></div>
+                    <div class="option"><span>${questions[index].options[1]}</span></div>
+                    <div class="option"><span>${questions[index].options[2]}</span></div>
+                    <div class="option"><span>${questions[index].options[3]}</span></div>`;
+    
+    optionList.innerHTML=optionTag;
+
+    const option=document.querySelectorAll('.option');
+    for(let i=0;i<option.length;i++){
+        option[i].setAttribute('onclick','optionSelected(this)');
+    }
+}
+
+function optionSelected(answer){
+    let userAnswer=answer.textContent;
+    let correctAnswer=questions[questionCount].answer;
+
+    if(userAnswer==correctAnswer){
+        answer.classList.add('correct');
+    }
+    else{
+        answer.classList.add('incorrect');
+    }
+}
+function questionCounter(index){
+    const questionTotal=document.querySelector('.question-total');
+    questionTotal.textContent=`${index} of ${questions.length} Questions`;
 }
